900d32e8d181b20d2b7b3c8a75c53fc38b9c4e7e
newsha400 <newsha400@gmail.com>
2018-03-28 16:06:13 -0400

    initial first setup

---
+++ src/main/java/bootcamp/model/Event.java
@@ -0,0 +1,30 @@
+package bootcamp.model;
+
+public class Event {
+	private String name;
+	private String description;
+	private String location;
+	
+	public Event() {
+		name= "concert";
+		description="two bands";
+		location="Georgia";
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
---
+++ src/test/groovy/bootcamp/EventSpec.groovy
@@ -0,0 +1,32 @@
+package bootcamp
+
+import bootcamp.model.Event
+import spock.lang.Specification
+
+class EventSpec extends Specification {
+
+	def "make an event"(){
+		given: "an event"
+		
+		when: "make an event"
+		Event concert = new Event();
+		then: "return the name"
+		concert.getName()=="concert";
+		and: "return description"
+		concert.getDescription()=="two bands";
+		and:"return location"
+		concert.getLocation()=="Georgia";
+	}
+	def "set location"(){
+		given: "a location"
+		String newLocation = "location"
+		and: "an event"
+		Event concert = new Event();
+		
+		when: "location is set"
+		concert.setLocation(newLocation)
+		then: "location changes "
+		concert.getLocation()== newLocation
+	}
+	
+}
