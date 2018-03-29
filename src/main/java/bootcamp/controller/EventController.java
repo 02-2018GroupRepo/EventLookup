5204ccd424c732e60fa0474bba332b5db1b6e395
syntel <syntel@PHX440G3-2815Z9.syntelorg.com>
2018-03-29 10:44:02 -0400

    before merge

--- src/main/java/bootcamp/controller/EventController.java
+++ src/main/java/bootcamp/controller/EventController.java
@@ -2,15 +2,24 @@ package bootcamp.controller;
 import bootcamp.model.Event;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
 
     
-    @RequestMapping("/event")
+   @RequestMapping("/event")
     public Event event() {
         return new Event();
     }
+    
+    @RequestMapping("/setEventLocation")
+    public Event event (@RequestParam(value = "location") String venueLocation) {
+    	Event event = new Event();
+		event.setLocation(venueLocation);
+    	return event;
+    }
+   
+    
 }
